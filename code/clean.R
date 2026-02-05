3244538433c11d6f5041b2f16fc6d502e60b3973
twixson <troy.wixson12@gmail.com>
2026-02-05 09:52:58 -0500

    Edits to clean.R and data_README.txt

--- code/clean.R
+++ code/clean.R
@@ -1,7 +1,15 @@
 # Cleaning data
 library(tidyverse)
-library(sf)
+library(sf) # to make a map of locations of study plots
+library(climateR) # to download climate data
+library(elevatr) # to download elevation data
 
+#####
+# Set values for which data to extract - options in data_README.txt
+#####
+eco_region        <- c("EASTERN TEMPERATE FORESTS", "NORTHERN FORESTS")
+data_source       <- "NPS" 
+include_elevation <- FALSE # !!!getting elevation data is slow!!!
 
 #####
 # Load datasets
@@ -14,10 +22,8 @@ plot_data <- read_csv("./data/SPCIS_plots.csv")
 #####
 plot_data <- plot_data |>
   # subset region and data source
-  # abbreviations in data_README.txt
-  subset(EcoRegionLevelI %in% 
-           c("EASTERN TEMPERATE FORESTS", "NORTHERN FORESTS")) |>
-  subset(Dataset %in% "NPS") |>  
+  subset(EcoRegionLevelI %in% eco_region) |>
+  subset(Dataset %in% data_source) |>  
   # keep most recent observation
   group_by(Plot) |>
   filter(Year == max(Year)) |> 
@@ -32,7 +38,7 @@ plot_data <- plot_data |>
 ###### The following bit of code inspects the plots with duplicate coords. 
 plot_data_repeated <- plot_data |>
   filter(coord_counts > 1)
-dim(plot_data)
+# dim(plot_data)
 # [1] 730  17
 hist(plot_data_repeated$coord_counts, breaks = 1:30)
 abline(h = c(11, 27), col = 2)
@@ -51,22 +57,75 @@ plot_data_repeated_unique_years <- plot_data_repeated |>
 plot_data <- plot_data |>
   filter(coord_counts == 1) |>
   ungroup() |>
-  select(-c(coords, coord_counts, # not needed any more
-            Original.Site, Original.Plot, # duplicate info
+  select(-c(coords, coord_counts,          # not needed any more
+            Original.Site, Original.Plot,  # duplicate info
             Zone, FuzzedCoord, Resampled)) # same value for every row
 
 ###Prep for pulling climate data###
 plot_sf <- st_as_sf(plot_data, coords=c("Long","Lat"))
 st_crs(plot_sf) <- st_crs(4326)
+plot_sf <- cbind(plot_sf, as.data.frame(st_coordinates(plot_sf)))
 
 # ########
 # # TW - map the coordinates: 
 # usa_states <- st_as_sf(maps::map("state", fill=TRUE, plot=FALSE))
-# ggplot() +
-#   geom_sf(data = usa_states, fill = NA, color = "gray20", size = 0.1) + # State borders
-#   geom_sf(data = plot_sf, alpha = 0.35) + # Your data on top
-#   coord_sf(xlim = c(-95, -65)) + # Ensures consistent projection - xlim limits to states we care about
+# ggplot() + # State lines
+#   geom_sf(data = usa_states, fill = NA, color = "gray20", size = 0.1) + 
+#   geom_sf(data = plot_sf, alpha = 0.35) + # plot data on top
+#   coord_sf(xlim = c(-95, -65)) + # limit to states we care about
 #   theme_bw() +
 #   labs(title = "Plot locations")
 # ########
+
+
+#### Download and process TerraClim normals###
+## Source: TerraClimate (https://www.climatologylab.org/terraclimate.html)
+## 30-year climate normals (1981-2010)
+## Citation: Abatzoglou et al., (2018) https://doi.org/10.1038/sdata.2017.191
+
+# set proj_lib to the terra package
+plib <- Sys.getenv("PROJ_LIB")
+prj <- system.file("proj", package = "terra")[1]
+Sys.setenv("PROJ_LIB" = prj)
+
+period <- "19812010"
+climate_vars <- c("tmax","tmin","soil","ppt","pet","aet","def","vpd")
+X <- data.frame(Plot = plot_sf$Plot)
+for (i in 1:length(climate_vars)){
+  climate_data <- getTerraClimNormals(plot_sf, 
+                                      climate_vars[i], period, 1:12)[[1]]
+  climate_data <- terra::extract(climate_data, plot_sf, ID=FALSE)
+    # !!!!Currently have a WARNING in the extract function b/c no spatVector data
+  
+  if (climate_vars[i] == "tmax"){ # map monthly data to annual values. 
+    X[[climate_vars[i]]] <- apply(climate_data[,5:9], 1, max)}
+  else if (climate_vars[i] == "tmin"){
+    X[[climate_vars[i]]] <- apply(climate_data[,5:9], 1, min)}
+  else if (climate_vars[i] %in% c("vpd")){
+    X[[climate_vars[i]]] <- apply(climate_data[,5:9], 1, mean)}
+  else if (climate_vars[i] %in% c("ppt","pet","aet","def","soil")){
+    X[[climate_vars[i]]] <- apply(climate_data[,5:9], 1, sum)}
+  else {
+    print("~~~ ERROR: variable not in climate_vars ~~~")
+  }
+}
+# reset proj_lib
+Sys.setenv("PROJ_LIB" = plib)
+
+if(include_elevation == TRUE){
+  # elevation (Source: https://rpubs.com/ials2un/elevationdata; Amazon Web Services Terrain Tiles)###
+  df_elev <- get_elev_raster(plot_sf, src="aws", z=8) #download elevation data
+    # !!!! This is very slow !!!!
+  elev_data <- terra::extract(df_elev, plot_sf) #extract elevation data at points
+  temp <- terra::terrain(df_elev, opt=c("slope", "aspect"), unit='degrees')
+  slope_aspect <- terra::extract(temp, plot_sf)
+  
+  # Merge all predictor variables
+  temp <- data.frame(elv=elev_data, slp=slope_aspect[,1], asp=slope_aspect[,2])
+  X<-cbind(X, temp)
+}
+
+
+X <- na.omit(X)
+
 
--- data/data_README.txt
+++ data/data_README.txt
@@ -1,6 +1,22 @@
 # data explanations
 
-# Dataset abbreviations
+# Eco Regions
+"EASTERN TEMPERATE FORESTS"
+"GREAT PLAINS"
+"MARINE WEST COAST FOREST"
+"MEDITERRANEAN CALIFORNIA"
+"NORTH AMERICAN DESERTS"
+"NORTHERN FORESTS"
+"NORTHWESTERN FORESTED MOUNTAINS" 
+"SOUTHERN SEMIARID HIGHLANDS"
+"TAIGA"
+"TEMPERATE SIERRAS"
+"TROPICAL WET FORESTS"
+"TUNDRA"
+"WATER"
+NA
+
+# Data source abbreviations
 NEON - NSF National Ecological Observatory Network
 FIA - USFS Forest Inventory Analysis
 WVNHP - West Virginia Natural Heritage Program
@@ -10,12 +26,12 @@ NPS - National Parks Service
 NWCA - EPA National Wetland Condition Assessment
 CVS - either Carolina Vegitation Survey or BLS Current Vegitation Survey (most lat and long are in the carolinas...)
 
-# Climate variable abbreviations
+# Climate variable abbreviations/definitions
 tmax - Max Temperature, average for month, units = C
 tmin - Min Temperature, average for month, units = C
-pet - Potential evapotranspiration, monthly total, units = mm (water movement from soil (evap) and plants (transp) to atmosphere)
 soil - Soil Moisture, total column - at end of month, units = mm
 ppt - Precipitation, monthly total, units = mm
+pet - Potential evapotranspiration, monthly total, units = mm (water movement from soil (evap) and plants (transp) to atmosphere)
 aet - Actual Evapotranspiration, monthly total, units = mm
 def - Climate Water Deficit, monthly total, units = mm (?pet - aet?)
 vpd - Vapor Pressure Deficit, average for month, units = kpa
