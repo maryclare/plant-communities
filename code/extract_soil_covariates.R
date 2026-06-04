d4d2c442b4860ac73043485f6f5d60ad364126ff
twixson <troy.wixson12@gmail.com>
2026-06-04 09:21:54 -0400

    undate context for data

--- code/extract_soil_covariates.R
+++ code/extract_soil_covariates.R
@@ -2,6 +2,8 @@ library(terra)
 
 plot_data <- readRDS("./data/nps_plot_data_as_SpatRaster.rds")
 covariate_files <- list.files("../soil_covariates/", pattern = "*.tif$")
+  # these files are available for download at https://scholarsphere.psu.edu/resources/ea4b6c45-9eba-4b89-aba6-ff7246880fb1
+  # see the soil_data_README.txt for more information. 
 covariates      <- data.frame(1:dim(plot_data))
 for(i in 1:length(covariate_files)){
   temp_file <- rast(paste0("../soil_covariates/", covariate_files[i]))
